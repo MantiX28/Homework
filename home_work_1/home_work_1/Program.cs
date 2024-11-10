b2babcb9cd6f584ad15c2c8e087ffd3912beeeaf
MantiX28 <MantiXprogramming28@gmail.com>
2024-11-11 00:29:32 +0330

    Add homework 1

---
+++ home_work_1/home_work_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("Enter First Number: ");
+            int num_1 = int.Parse(Console.ReadLine());
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Enter Second Number: ");
+            int num_2 = int.Parse(Console.ReadLine());
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine($"\nThe Average Is {(num_1+num_2)/2}");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
